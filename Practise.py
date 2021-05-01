19ff5da0821e94a403fa5c0f53fcde535131cb01
natalied <nataliedoyle@scopeeyecare.com>
2021-05-01 14:33:35 +0100

    Mod 1 practice

--- Practise.py
+++ Practise.py
@@ -71,3 +71,15 @@ fig, ax.plot(austin["DATE"],austin["MLY-DUTR-NORMAL"],yerr=austin["MLY-DUTR-STDD
 fig, ax.plot(seattle["DATE"],seattle["MLY-DUTR-NORMAL"],yerr=austin["MLY-DUTR-STDDEV"])
 ax.set_ylabel=("Temperature")
 plt.show()
+
+
+# gold medal stacked chart
+medals=pd.read_csv("medals_by_country_2016.csv",index_col=0)
+fig,ax =plt.subplots()
+ax.bar(medals.index,medals["Gold"],label="Gold")
+ax.bar(medals.index, medals["Silver"],bottom=medals["Gold"],label="Silver")
+ax.bar(medals.index, medals["Bronze"],bottom=medals["Gold"]+medals["Silver"],label="Bronze")
+ax.set_ylabel("Number of Medals")
+fig.set_size_inches([18,10])
+ax.legend()
+fig.savefig("gold_medals_best.jpg")
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 # Example 1, do not modify!
 import matplotlib.pyplot as plt
 import pandas as pd
+from datetime import datetime
 
+today=pd.Timestamp
+print(today)
 
-medals=pd.read_csv("medals_by_country_2016.csv",index_col=0)
-fig,ax =plt.subplots()
-ax.bar(medals.index,medals["Gold"],label="Gold")
-ax.bar(medals.index, medals["Silver"],bottom=medals["Gold"],label="Silver")
-ax.bar(medals.index, medals["Bronze"],bottom=medals["Gold"]+medals["Silver"],label="Bronze")
-ax.set_ylabel("Number of Medals")
-fig.set_size_inches([18,10])
-ax.legend()
-fig.savefig("gold_medals_best.jpg")
+index=pd.date_range(start='2020-1-1',periods=12, freq='M')
+print(index)
