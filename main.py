45bd2c2c49a376549567016a236529d57ed9d888
natalied <nataliedoyle@scopeeyecare.com>
2021-05-10 21:34:08 +0100

    Mod 1 practice

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 
 Stock_prices=pd.read_csv("HistoricalQuotes - Copy.csv",index_col=0)
 print(Stock_prices.info())
-Stock_top=Stock_prices.head(50)
+Stock_top=Stock_prices.head(20)
 
 fig,ax=plt.subplots()
 ax.plot(Stock_top.index,Stock_top["close"])
+fig.autofmt_xdate()
 print(ax)
 plt.show()
